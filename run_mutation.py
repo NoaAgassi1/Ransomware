f1ab06e11f5f00ba6c04bd21581102e8df493d0d
NOA AGASSI <agasi.noa@msmail.ariel.ac.il>
2025-08-09 13:40:00 +0300

    Update run_mutation.py

--- run_mutation.py
+++ run_mutation.py
@@ -15,10 +15,5 @@ mutator.reverse_and_scramble()
 #         self.scramble_one_line()
 #         self.encrypt_with_fernet()
 
-# encrypt_with_fernet() – מצפינה קובץ אחד באמצעות Fernet (אלגוריתם סימטרי מודרני עם אימות).
-# encrypt_with_aes() – מצפינה קובץ אחד עם AES במצב EAX (כולל אימות באמצעות tag).
-#corrupt_to_non_ascii() – מחליפה את תוכן שני קבצים אקראיים בטקסט אקראי שאינו ASCII.
-# encrypt_line_simulation() – מחליפה שורה אקראית בשני קבצים בטקסט ג'יבריש שנראה כמו פלט מוצפן.
-# reverse_and_scramble() – הופכת כל מילה בשורות לשיקוף ומערבבת מילים בשורה אקראית.
-# scramble_one_line() – מערבבת את התווים בשורה אחת אקראית מתוך קובץ אקראי.
-# encrypt_with_des() – מצפינה קובץ אחד עם DES במצב ECB וריפוד רווחים (לצורכי הדמיה בלבד).
+
+
